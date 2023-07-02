eb5fdb58faf541ae0a8791f136e0f871da0b566e
airRnot1106 <yngw.rintaro@gmail.com>
2023-07-02 11:18:02 +0900

    env: add production files

--- bot/src/server/index.ts
+++ bot/src/server/index.ts
@@ -1,6 +1,7 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
+import prisma from '@/libs/prisma';
 import { root } from '@/server/api';
 import { history } from '@/server/api/history';
 import { queue } from '@/server/api/queue';
@@ -20,3 +21,17 @@ serve(route, (info) => {
 });
 
 export type ApiType = typeof route;
+
+(async () => {
+  await prisma.setting.upsert({
+    where: { key: 'SETTING' },
+    update: {},
+    create: {
+      key: 'SETTING',
+      language: 'EN',
+    },
+  });
+})().catch((error) => {
+  // eslint-disable-next-line no-console
+  console.error(error);
+});
